03cf0fbf8df5c2448fae3f2a177d9f2dd5a1b08d
MrsViktoRs <Vhomutov167@gmail.com>
2024-11-28 03:43:26 +0300

    add-function-ordersMessage

--- src/components/adminPanel/AdminPanel.jsx
+++ src/components/adminPanel/AdminPanel.jsx
@@ -13,6 +13,46 @@ import './adminPanel.css'
 
 export default function AdminPanel() {
     const [selectView, setSelectView] = useState('orderView');
+    const [messages, setMessages] = useState([]);
+    const [newMessages, setNewMessages] = useState([]);
+    
+    const fetchMessages = async () => {
+        const response = await fetch('http://127.0.0.1:8000/api/v1/messages/poll/');
+        const data = await response.json();
+        if (data.length <= messages.length) {
+            console.log('Новых сообщений нет');
+            setMessages(data);
+            return
+        }
+        else {
+            setMessages(data);
+            if (data.filter((message) => message.role === 'orders').length > 0) {
+                const item = data.filter((message) => message.role === 'orders');
+                const lastMessage = item[item.length - 1];
+                setNewMessages(item);
+                alert(`Новая заявка от ${lastMessage.user.name} ${lastMessage.user.surname}!`);
+            }
+            else if (data.filter((message) => message.role === 'appeal').length > 0) {
+                const item = data.filter((message) => message.role === 'appeal');
+                const lastMessage = item[item.length - 1];
+                setNewMessages(item);
+                alert(`Новое обращение от ${lastMessage.user.name} ${lastMessage.user.surname}!`);
+            }
+            else if (data.filter((message) => message.role === 'help').length > 0) {
+                const item = data.filter((message) => message.role === 'help');
+                const lastMessage = item[item.length - 1];
+                setNewMessages(item);
+                alert(`Новое обращение от ${lastMessage.user.name} ${lastMessage.user.surname}!`);
+            }
+            return
+        }
+    };
+
+    useEffect(() => {
+        fetchMessages();
+        const interval = setInterval(fetchMessages, 5000);
+        return () => clearInterval(interval);
+    }, [newMessages]);
 
     function handleViewClick(view) {
         setSelectView(view);
@@ -58,7 +98,7 @@ export default function AdminPanel() {
                 <>
                 <Headadmin />
                 <Mainbuttons onClick={handleViewClick}/>
-                <OrdersMessage />
+                <OrdersMessage messages={messages}/>
                 </>
                 )
             case 'reportView':
--- src/components/ordersMessage/OrdersMessage.jsx
+++ src/components/ordersMessage/OrdersMessage.jsx
@@ -2,99 +2,36 @@ import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import './ordersMessage.css';
 
-export default function OrdersMessage() {
+export default function OrdersMessage({ messages }) {
     const [selectView, setSelectView] = useState('order');
+    const [message, setMessage] = useState(messages);
     const [history, setHistory] = useState(false);
-    const [data, setData] = useState([]);
+    const [historyData, setHistoryData] = useState([]);
+    const [selectMess, setSelectMess] = useState(messages);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [isModalOpenHistory, setIsModalOpenHistory] = useState(false);
 
     const arData = {
         order: [
-            {
-                message: 'Тестовый текст без информации',
-                user: 'Иванов Иван Иванович',
-                dt: {
-                    date: '12.12.2022',
-                    time: '12:12'
-                },
-                status: true,
-            },
-            {
-                message: 'Тестовый',
-                user: 'WOWOWOW',
-                dt: {
-                    date: '12.12.2022',
-                    time: '12:12'
-                },
-                status: false,
-            },
-            {
-                message: 'Что-то понятное',
-                user: 'Такой-то',
-                dt: {
-                    date: '12.12.2022',
-                    time: '12:12'
-                },
-                status: true,
-            },
-            {
-                message: 'Очень большой текст на свякие штуки для информативности и помощи в стилизации. И текст информации',
-                user: 'Ив Ин Ич',
-                dt: {
-                    date: '12.12.2022',
-                    time: '12:12'
-                },
-                status: true,
-            },
-            {
-                message: 'Писать не буду',
-                user: 'Иванов Иван Иванович',
-                dt: {
-                    date: '12.12.2022',
-                    time: '12:12'
-                },
-                status: true,
-            }
+            message.filter((item) => item.role === 'orders'),
         ],
         appeal: [
-            {
-                message: 'Очень большой текст на свякие штуки для информативности и помощи в стилизации. И текст информации',
-                user: 'Ив Ин Ич',
-                dt: {
-                    date: '12.12.2022',
-                    time: '12:12'
-                },
-                status: true,
-            },
-            {
-                message: 'Писать не буду',
-                user: 'Иванов Иван Иванович',
-                dt: {
-                    date: '12.12.2022',
-                    time: '12:12'
-                },
-                status: true,
-            }
+            message.filter((item) => item.role === 'appeal'),
         ],
-        // help: [
-        //     {
-        //         message: 'Потерялся в лесу, не хотел... так получилось... ПОМОГИТЕ!!!',
-        //         user: 'Ив Ин Ич',
-        //         dt: {
-        //             date: '12.12.2022',
-        //             time: '12:12'
-        //         },
-        //         status: true,
-        //     },
-        //     {
-        //         message: 'Писать не буду больше, слишком это тяжело',
-        //         user: 'Ива Ива Ива',
-        //         dt: {
-        //             date: '12.12.2022',
-        //             time: '12:12'
-        //         },
-        //         status: true,
-        //     }
-        // ]
+        help: [
+            message.filter((item) => item.role === 'help'),
+        ]
+    }
+
+    const fetchMessages = async () => {
+        const response = await fetch('http://127.0.0.1:8000/api/v1/messages/poll/');
+        setMessage(await response.json());
+    }
+
+    const fetchHistory = async () => {
+        const response = await fetch('http://127.0.0.1:8000/api/v1/all_history/');
+        const data = await response.json();
+        setHistoryData(data);
     }
 
     const handleViewClick = (view) => {
@@ -102,30 +39,62 @@ export default function OrdersMessage() {
     }
 
     const handleHistoryClick = () => {
+        fetchHistory();
         setHistory(!history);
     }
 
+    const handleCloseMessage = async (id) => {
+        const response = await axios.patch(`http://127.0.0.1:8000/api/v1/all_history/${id}/`);
+        const data = response;
+        fetchHistory();
+        fetchMessages();
+        setIsModalOpen(false);
+        console.log(data);
+    }
+
+    const handleOpenMessage = (id) => {
+        const item = message.filter((item) => item.id === id);
+        setSelectMess(item[0]);
+        setIsModalOpen(true);
+        console.log(selectMess);
+    }
+
+    const handleOpenMessageHistory = (id) => {
+        const item = historyData.filter((item) => item.id === id);
+        setSelectMess(item[0]);
+        setIsModalOpenHistory(true);
+        console.log(selectMess);
+    }
+
+    const handleCloseModalHistory = () => {
+        setIsModalOpenHistory(false);
+    }
+
+
+    const handleCloseModal = () => {
+        setIsModalOpen(false);
+    }
+
     function renderData(mode) {
-        const items = arData[mode]; // Получаем данные для текущего режима
-    
+        const items = arData[mode];
         if (!items) {
             return (
                 <div className="noData">Данные отсутствуют...</div>
             )
         }
     
-        return items.map((item, index) => (
-            <div className="row" key={index}>
+        return items[0].map((item, index) => (
+            <div className="row" key={index} onClick={() => {handleOpenMessage(item.id)}}>
                 <div className="message">{item.message}</div>
-                <div className="user">{item.user}</div>
-                <div className="dt">{item.dt.time} || {item.dt.date}</div>
-                <button className="closeNote">Закр. заявку</button>
+                <div className="user">{item.user.surname}  {item.user.name}</div>
+                <div className="dt">{item.formatted_dt}</div>
+                <button className="closeNote" onClick={() => {handleCloseMessage(item.id)}}>Закр. заявку</button>
             </div>
         ));
     }
 
     function renderHistory(mode) {
-        const items = arData[mode]; // Получаем данные для текущего режима
+        const items = arData[mode];
     
         if (!items) {
             return (
@@ -133,17 +102,58 @@ export default function OrdersMessage() {
             )
         }
     
-        return items.map((item, index) => (
-            <div className="rowHistory" key={index} style={{color: '#909090'}}>
+        return historyData.map((item, index) => (
+            <div className="rowHistory" key={index} style={{color: '#909090'}} onClick={() => {handleOpenMessageHistory(item.id)}}>
                 <div className="message">{item.message}</div>
-                <div className="user">{item.user}</div>
-                <div className="dt">{item.dt.time} || {item.dt.date}</div>
+                <div className="user"></div>
+                <div className="dt">{item.formatted_dt}</div>
             </div>
         ));
     }
 
+    function renderModal(isModalOpen) {
+        if (isModalOpen) {
+            return (
+                <div className="modalUser">
+                    <span className="modalHead">
+                        <span className="modalRelated">{selectMess.user.address}</span>
+                        <span className="modalDate">{selectMess.formatted_dt}</span>
+                        <span className="modalAction">
+                            <button className="modalCloseBtn" onClick={() => {handleCloseModal()}}>Вернуться</button>
+                            <button className="modalCloseMessBtn" onClick={() => {handleCloseMessage(selectMess.id)}}>Закр. заявку</button>
+                        </span>
+                    </span>
+                    <div className="modalContent">{selectMess.message}</div>
+                </div>
+            )
+        }
+    }
+
+    function renderHistoryModal(isModalOpenHistory) {
+        if (isModalOpenHistory) {
+            return (
+                <div className="modalUser">
+                    <span className="modalHead">
+                        <span className="modalRelated">{selectMess.user.address}</span>
+                        <span className="modalDate">{selectMess.formatted_dt}</span>
+                        <span className="modalAction">
+                            <button className="modalCloseBtn" onClick={() => {handleCloseModalHistory()}}>Вернуться</button>
+                        </span>
+                    </span>
+                    <div className="modalContent">{selectMess.message}</div>
+                </div>
+            )
+        }
+    }
+
+    useEffect(() => {
+        fetchMessages();
+    }, [selectMess, message]);
+
     return (
         <div className="ordersMessage">
+            {renderModal(isModalOpen)}
+            {renderHistoryModal(isModalOpenHistory)}
             <div className="actionOrdersMessage">
                 <div className="views">
                     <span id="orderMess" className="viewBtn" onClick={() => {handleViewClick('order')}} style={{backgroundColor: selectView === 'order' ? '#506365' : '#7A9E9F'}}>Заявки</span>
